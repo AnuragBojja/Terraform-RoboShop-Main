bbcdad09907c1a2fc2c904ca95427ab800d8cc4e
AnuragBojja <anuragbojja19@gmail.com>
2026-05-13 12:54:15 -0500

    docker

--- 100-docker-instance/docker-installation.sh
+++ 100-docker-instance/docker-installation.sh
@@ -7,7 +7,7 @@ xfs_growfs /var
 #installing docker
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker 
 systemctl enable docker 
 usermod -aG docker ec2-user
